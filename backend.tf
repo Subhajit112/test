c7d6f2fbefc9ce8d0a85b09c7ac6bf5b9f778b9f
subhajit <subhajitsadhu5239@gmail.com>
2024-12-03 20:42:22 +0530

    new1

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = var.azurerm_resource_group.rg
-    storage_account_name = azurerm_storage_account.mystorage.name
-    container_name       = azurerm_storage_container.tfstate.name
+    storage_account_name = "storage"
+    container_name       = "tfstate"
     key                  = "test.terraform.tfstate"
   }
 }
